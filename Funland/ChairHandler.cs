6efc46ec1f4233509391c62b2d264beff7454e1f
ToastedOven <49164897+ToastedOven@users.noreply.github.com>
2022-05-08 17:24:38 -0400

    RPS anims done

---
+++ Funland/ChairHandler.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace TitanFall2Emotes
+{
+    class ChairHandler : MonoBehaviour
+    {
+        internal GameObject chair;
+        bool check = true;
+        internal Vector3 pos;
+        internal Vector3 rot;
+        internal Vector3 scal;
+        bool check2 = false;
+        bool check3 = false;
+        internal BoneMapper mapper;
+        float timer = 0;
+        int whenToEmote = 0;
+
+        void Start()
+        {
+            foreach (var item in GetComponentInChildren<SkinnedMeshRenderer>().materials)
+            {
+                item.shader = TF2Plugin.defaultShader;
+                item.shaderKeywords = new string[] { "DITHER" };
+            }
+            whenToEmote = UnityEngine.Random.Range(15, 25);
+        }
+        void Update()
+        {
+            if (check)
+            {
+                timer += Time.deltaTime;
+            }
+            if (timer > whenToEmote)
+            {
+                timer = 0;
+                whenToEmote = UnityEngine.Random.Range(15, 25);
+                switch (UnityEngine.Random.Range(0, 3))
+                {
+                    case 0:
+                        mapper.preserveParent = true;
+                        mapper.preserveProps = true;
+                        DebugClass.Log($"burp");
+                        mapper.PlayAnim("engiRanchoBurp", 0);
+                        AkSoundEngine.PostEvent("Play_RanchoBurp", mapper.gameObject);
+                        break;
+                    case 1:
+                        mapper.preserveParent = true;
+                        mapper.preserveProps = true;
+                        DebugClass.Log($"bigDrink");
+                        mapper.PlayAnim("engiRanchoBigDrink", 0);
+                        AkSoundEngine.PostEvent("Play_RanchoLong", mapper.gameObject);
+                        GetComponentInChildren<Animator>().SetBool("BigDrink", true);
+                        break;
+                    case 2:
+                        mapper.preserveParent = true;
+                        mapper.preserveProps = true;
+                        DebugClass.Log($"quickDrink");
+                        mapper.PlayAnim("engiRanchoQuickDrink", 0);
+                        AkSoundEngine.PostEvent("Play_RanchoQuick", mapper.gameObject);
+                        GetComponentInChildren<Animator>().SetBool("SmallDrink", true);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (timer > 3)
+            {
+                GetComponentInChildren<Animator>().SetBool("BigDrink", false);
+                GetComponentInChildren<Animator>().SetBool("SmallDrink", false);
+            }
+            if (check && !chair)
+            {
+                check = false;
+                StartCoroutine(spinThenDestroy());
+                GetComponentInChildren<Animator>().SetBool("Breaking", true);
+                gameObject.transform.SetParent(null);
+                gameObject.transform.position = pos;
+                gameObject.transform.localEulerAngles = rot;
+                gameObject.transform.localScale = scal;
+                scal *= 1.333f;
+                AkSoundEngine.PostEvent("Stop_Rancho", mapper.gameObject);
+                AkSoundEngine.PostEvent("Play_RanchoClose", gameObject);
+            }
+            if (check3)
+            {
+                gameObject.transform.localScale = Vector3.Slerp(gameObject.transform.localScale, Vector3.zero, Time.deltaTime * 5);
+            }
+            else if (check2)
+            {
+                gameObject.transform.localScale = Vector3.Slerp(gameObject.transform.localScale, scal, Time.deltaTime * 30);
+            }
+        }
+
+        IEnumerator spinThenDestroy()
+        {
+            yield return new WaitForSeconds(3.5f);
+            check2 = true;
+            yield return new WaitForSeconds(.15f);
+            check3 = true;
+            yield return new WaitForSeconds(1f);
+            Destroy(gameObject);
+        }
+    }
+}
--- Funland/TF2Plugin.cs
+++ Funland/TF2Plugin.cs
@@ -34,7 +34,7 @@ namespace TitanFall2Emotes
             Assets.PopulateAssets();
             Assets.AddSoundBank("tf2.bnk");
             Assets.LoadSoundBanks();
-            AddAnimation("Engi/Rancho/RanchoRelaxo", "Rancho", "Engi/Rancho/RanchoLoop", false, false);
+            Rancho();
             RPS();
             Register();
             CustomEmotesAPI.animChanged += CustomEmotesAPI_animChanged;
@@ -44,6 +44,13 @@ namespace TitanFall2Emotes
         {
             NetworkingAPI.RegisterMessageType<SyncRandomEmoteToClient>();
             NetworkingAPI.RegisterMessageType<SyncRandomEmoteToHost>();
+        }
+        public void Rancho()
+        {
+            AddAnimation("Engi/Rancho/RanchoRelaxo", "Rancho", "Engi/Rancho/engiRanchoPassive", false, false);
+            AddAnimation("Engi/Rancho/engiRanchoBurp", "", "Engi/Rancho/engiRanchoPassive", false, false, false);
+            AddAnimation("Engi/Rancho/engiRanchoBigDrink", "", "Engi/Rancho/engiRanchoPassive", false, false, false);
+            AddAnimation("Engi/Rancho/engiRanchoQuickDrink", "", "Engi/Rancho/engiRanchoPassive", false, false, false);
         }
         public void RPS()
         {
@@ -69,6 +76,157 @@ namespace TitanFall2Emotes
             emote = AddHiddenAnimation(new string[] { "Engi/RPS/EngiRPS_SLose" }, new string[] { "" }, "");
             RPS_Loss_Emotes.Add(emote);
 
+
+            emote = AddHiddenAnimation(new string[] { "Demo/RPS/DemoRPS_Start" }, new string[] { "Demo/RPS/DemoRPS_Loop" }, new string[] { "" }, "", new JoinSpot[] { new JoinSpot("RPSJoinSpot", new Vector3(0, 0, 1.5f)) });
+            RPS_Start_Emotes.Add(emote);               
+                                                       
+            emote = AddHiddenAnimation(new string[] { "Demo/RPS/DemoRPS_RWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);                 
+            emote = AddHiddenAnimation(new string[] { "Demo/RPS/DemoRPS_RLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);                
+                                                       
+            emote = AddHiddenAnimation(new string[] { "Demo/RPS/DemoRPS_PWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);                 
+            emote = AddHiddenAnimation(new string[] { "Demo/RPS/DemoRPS_PLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);                
+                                                       
+            emote = AddHiddenAnimation(new string[] { "Demo/RPS/DemoRPS_SWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);                 
+            emote = AddHiddenAnimation(new string[] { "Demo/RPS/DemoRPS_SLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+
+            emote = AddHiddenAnimation(new string[] { "Soldier/RPS/SoldierRPS_Start" }, new string[] { "Soldier/RPS/SoldierRPS_Loop" }, new string[] { "" }, "", new JoinSpot[] { new JoinSpot("RPSJoinSpot", new Vector3(0, 0, 1.5f)) });
+            RPS_Start_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Soldier/RPS/SoldierRPS_RWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Soldier/RPS/SoldierRPS_RLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Soldier/RPS/SoldierRPS_PWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Soldier/RPS/SoldierRPS_PLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Soldier/RPS/SoldierRPS_SWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Soldier/RPS/SoldierRPS_SLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+
+            emote = AddHiddenAnimation(new string[] { "Heavy/RPS/HeavyRPS_Start" }, new string[] { "Heavy/RPS/HeavyRPS_Loop" }, new string[] { "" }, "", new JoinSpot[] { new JoinSpot("RPSJoinSpot", new Vector3(0, 0, 1.5f)) });
+            RPS_Start_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Heavy/RPS/HeavyRPS_RWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Heavy/RPS/HeavyRPS_RLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Heavy/RPS/HeavyRPS_PWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Heavy/RPS/HeavyRPS_PLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Heavy/RPS/HeavyRPS_SWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Heavy/RPS/HeavyRPS_SLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+
+            emote = AddHiddenAnimation(new string[] { "Medic/RPS/MedicRPS_Start" }, new string[] { "Medic/RPS/MedicRPS_Loop" }, new string[] { "" }, "", new JoinSpot[] { new JoinSpot("RPSJoinSpot", new Vector3(0, 0, 1.5f)) });
+            RPS_Start_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Medic/RPS/MedicRPS_RWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Medic/RPS/MedicRPS_RLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Medic/RPS/MedicRPS_PWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Medic/RPS/MedicRPS_PLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Medic/RPS/MedicRPS_SWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Medic/RPS/MedicRPS_SLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+
+            emote = AddHiddenAnimation(new string[] { "Pyro/RPS/PyroRPS_Start" }, new string[] { "Pyro/RPS/PyroRPS_Loop" }, new string[] { "" }, "", new JoinSpot[] { new JoinSpot("RPSJoinSpot", new Vector3(0, 0, 1.5f)) });
+            RPS_Start_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Pyro/RPS/PyroRPS_RWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Pyro/RPS/PyroRPS_RLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Pyro/RPS/PyroRPS_PWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Pyro/RPS/PyroRPS_PLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Pyro/RPS/PyroRPS_SWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Pyro/RPS/PyroRPS_SLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+
+            emote = AddHiddenAnimation(new string[] { "Scout/RPS/ScoutRPS_Start" }, new string[] { "Scout/RPS/ScoutRPS_Loop" }, new string[] { "" }, "", new JoinSpot[] { new JoinSpot("RPSJoinSpot", new Vector3(0, 0, 1.5f)) });
+            RPS_Start_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Scout/RPS/ScoutRPS_RWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Scout/RPS/ScoutRPS_RLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Scout/RPS/ScoutRPS_PWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Scout/RPS/ScoutRPS_PLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Scout/RPS/ScoutRPS_SWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Scout/RPS/ScoutRPS_SLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+
+            emote = AddHiddenAnimation(new string[] { "Sniper/RPS/SniperRPS_Start" }, new string[] { "Sniper/RPS/SniperRPS_Loop" }, new string[] { "" }, "", new JoinSpot[] { new JoinSpot("RPSJoinSpot", new Vector3(0, 0, 1.5f)) });
+            RPS_Start_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Sniper/RPS/SniperRPS_RWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Sniper/RPS/SniperRPS_RLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Sniper/RPS/SniperRPS_PWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Sniper/RPS/SniperRPS_PLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Sniper/RPS/SniperRPS_SWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Sniper/RPS/SniperRPS_SLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+
+            emote = AddHiddenAnimation(new string[] { "Spy/RPS/SpyRPS_Start" }, new string[] { "Spy/RPS/SpyRPS_Loop" }, new string[] { "" }, "", new JoinSpot[] { new JoinSpot("RPSJoinSpot", new Vector3(0, 0, 1.5f)) });
+            RPS_Start_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Spy/RPS/SpyRPS_RWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Spy/RPS/SpyRPS_RLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Spy/RPS/SpyRPS_PWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Spy/RPS/SpyRPS_PLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
+
+            emote = AddHiddenAnimation(new string[] { "Spy/RPS/SpyRPS_SWin" }, new string[] { "" }, "");
+            RPS_Win_Emotes.Add(emote);
+            emote = AddHiddenAnimation(new string[] { "Spy/RPS/SpyRPS_SLose" }, new string[] { "" }, "");
+            RPS_Loss_Emotes.Add(emote);
         }
 
         private void CustomEmotesAPI_emoteSpotJoined_Body(GameObject emoteSpot, BoneMapper joiner, BoneMapper host)
@@ -127,10 +285,11 @@ namespace TitanFall2Emotes
                 g.transform.localScale = new Vector3(1.175f, 1.175f, 1.175f);
                 mapper.ScaleProps();
                 mapper.props.RemoveAt(mapper.props.Count - 1);
-                ChairDissasembler chair = g.AddComponent<ChairDissasembler>();
+                ChairHandler chair = g.AddComponent<ChairHandler>();
                 chair.pos = g.transform.position;
                 chair.rot = g.transform.eulerAngles;
                 chair.scal = g.transform.lossyScale;
+                chair.mapper = mapper;
 
                 g = new GameObject();
                 g.name = "RanchoRelaxoProp";
@@ -141,6 +300,10 @@ namespace TitanFall2Emotes
                 mapper.AssignParentGameObject(g, true, true, true, false, false);
                 chair.chair = g;
             }
+            //if (newAnimation == "engiRanchoBurp" || newAnimation == "engiRanchoBigDrink" || newAnimation == "engiRanchoQuickDrink")
+            //{
+            //    mapper.AssignParentGameObject(mapper.props[0], true, true, true, false, false);
+            //}
             if (newAnimation == "Rock Paper Scissors")
             {
                 if (NetworkServer.active)
@@ -163,6 +326,10 @@ namespace TitanFall2Emotes
         internal void AddAnimation(string AnimClip, string wwise, string AnimClip2ElectricBoogaloo, bool dimAudio, bool sync)
         {
             CustomEmotesAPI.AddCustomAnimation(Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/{AnimClip}.anim"), true, $"Play_{wwise}", $"Stop_{wwise}", secondaryAnimation: Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/{AnimClip2ElectricBoogaloo}.anim"), dimWhenClose: dimAudio, syncAnim: sync, syncAudio: sync);
+        }
+        internal void AddAnimation(string AnimClip, string wwise, string AnimClip2ElectricBoogaloo, bool dimAudio, bool sync, bool visibility)
+        {
+            CustomEmotesAPI.AddCustomAnimation(Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/{AnimClip}.anim"), true, $"Play_{wwise}", $"Stop_{wwise}", secondaryAnimation: Assets.Load<AnimationClip>($"@ExampleEmotePlugin_badassemotes:assets/{AnimClip2ElectricBoogaloo}.anim"), dimWhenClose: dimAudio, syncAnim: sync, syncAudio: sync, visible: visibility);
         }
         internal string AddHiddenAnimation(string[] AnimClip, string[] AnimClip2ElectricBoogaloo, string[] wwise, string stopWwise, JoinSpot[] joinSpots)
         {
